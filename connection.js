6787f8900a25ae556fda5585593d4a9f0628d420
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2021-12-19 14:18:29 +0530

    Add files via upload

--- connection.js
+++ connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
     try{
-        mongoose.connect('mongodb://localhost:27017/b262-ecommerce', {useNewUrlParser : true, useUnifiedTopology:true})
+        mongoose.connect('mongodb+srv://sivaranjini:fFiF1ppDwqCI5T5p@cluster0.sdfhf.mongodb.net/b262-ecommerce?retryWrites=true&w=majority', {useNewUrlParser : true, useUnifiedTopology:true})
     } catch(err) {
         console.log(err);
         process.exit();
     }
 }
+
